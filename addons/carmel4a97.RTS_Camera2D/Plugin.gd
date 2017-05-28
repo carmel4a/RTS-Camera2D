9dab7ad21ad2c65043743afc1fc3363ce60ac588
Daniel Lewan <vision360.daniel@gmail.com>
2017-05-28 15:26:21 +0200

    fix formatting in Plugin.gd

--- addons/carmel4a97.RTS_Camera2D/Plugin.gd
+++ addons/carmel4a97.RTS_Camera2D/Plugin.gd
@@ -23,9 +23,7 @@ tool
 extends EditorPlugin
 
 func _enter_tree():
-	
 	 add_custom_type("RTS-Camera2D", "Camera2D", preload("RTS-Camera2D.gd"), preload("node_icon.png"))
 
 func _exit_tree():
-	
 	remove_custom_type("RTS-Camera2D")
